0c597bd61c005a5df43ec4462225a1cde2b73508
Michael <kobelu1990@gmail.com>
2020-04-02 11:30:02 +0800

    seprate settings

--- garbage_sorting/settings/common.py
+++ garbage_sorting/settings/common.py
@@ -13,19 +13,19 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cbiz12hlq#44a1b71cxphiz7l2=%gly2=5l9-x0ll*-5*1u5&s'
+# SECRET_KEY = 'cbiz12hlq#44a1b71cxphiz7l2=%gly2=5l9-x0ll*-5*1u5&s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
---
+++ garbage_sorting/settings/production.py
@@ -0,0 +1,8 @@
+
+from .common import *
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['192.168.5.6', 'www.reng.me', 'reng.me']
